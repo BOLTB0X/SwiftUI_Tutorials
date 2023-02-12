65b11828e81bf4eaa473f0ab0af8f820c29d7c69
BOLTB0X <khunlee0508@gmail.com>
2023-02-12 16:46:06 +0900

    to section 3 commit

--- Ch01_SwiftUI Essentials/Landmarks/Landmarks/ContentView.swift
+++ Ch01_SwiftUI Essentials/Landmarks/Landmarks/ContentView.swift
@@ -9,11 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack { //스택 뷰로 수직으로 쌓임
             Text("Turtle Rock")
-                .font(.title)
+                .font(.title) // 제목
+            
+            HStack { // 수평으로 쌓임
+                Text("Joshua Tree Nation")
+                    .font(.subheadline)
+                
+                Spacer() // 공백처리
+                // 기존 쌓인 인스턴스들의 간격을 자동으로 계산해줌
+                
+                Text("California")
+                    .font(.subheadline)
+            }
         }
-        .padding()
+        .padding() // 간격
     }
 }
 
