fd4265d81e855e31ae4e12b14d93ff1995da93c4
BOLTB0X <khunlee0508@gmail.com>
2023-02-12 16:09:34 +0900

    first commit

---
+++ Ch01_SwiftUI Essentials/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by KyungHeon Lee on 2023/02/11.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
