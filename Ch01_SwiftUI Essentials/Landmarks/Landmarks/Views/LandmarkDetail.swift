800eaac14c1f99c33d3d89a4955d4d8fe25b93c9
BOLTB0X <khunlee0508@gmail.com>
2023-02-15 23:46:52 +0900

    section 7

--- Ch01_SwiftUI Essentials/Landmarks/Landmarks/Views/LandmarkDetail.swift
+++ Ch01_SwiftUI Essentials/Landmarks/Landmarks/Views/LandmarkDetail.swift
@@ -8,48 +8,63 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    var landmark: Landmark
+    
     var body: some View {
-        VStack { // Vstack으로 감싸줌
+        ScrollView { // 변경
+//        VStack { // Vstack으로 감싸줌
             // 맵뷰 추가
-            MapView()
-                .frame(height: 300) // framedmf 통해 높이 조정
+            // 수정
+            MapView(coordinate: landmark.locationCoordinate)                .frame(height: 300) // framedmf 통해 높이 조정
             
             // 아까만든 원이미지
-            CircleImage()
+            CircleImage(image: landmark.image)
+
             // 스택들이 자동 비율 계산으로의 위치보단 원하는 위치로 옮기고 싣다면
                 .offset(y: -130)
                 .padding(.bottom, -130)
             
             // 하단 text뷰들
             VStack(alignment: .leading) { //스택 뷰로 수직으로 쌓임 정렬 기준을 왼쪽으로
-                Text("Turtle Rock")
+                Text(landmark.name) // 수정
                     .font(.title) // 제목
                 
                 HStack { // 수평으로 쌓임
-                    Text("Joshua Tree Nation")
-                        .font(.subheadline)
-                    
-                    Spacer() // 공백처리
-                    // 기존 쌓인 인스턴스들의 간격을 자동으로 계산해줌
-                    
-                    Text("California")
-                        .font(.subheadline)
+                    Text(landmark.park)
+                    Spacer()
+                    Text(landmark.state)
+//                    Text("Joshua Tree Nation")
+//                        .font(.subheadline)
+//
+//                    Spacer() // 공백처리
+//                    // 기존 쌓인 인스턴스들의 간격을 자동으로 계산해줌
+//
+//                    Text("California")
+//                        .font(.subheadline)
                 }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
                 Divider() // 구분선 추가
                 
-                Text("About Turtle Rock")
+                Text("About \(landmark.name)")
                     .font(.title2)
-                Text("Descriptive text goes here.")
+                Text(landmark.description)
+//                Text("About Turtle Rock")
+//                    .font(.title2)
+//                Text("Descriptive text goes here.")
             }
             .padding() // 간격
             
-            Spacer()
+//            Spacer()
         }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkDetail()
+        LandmarkDetail(landmark: landmarks[0])
     }
 }
--- Ch01_SwiftUI Essentials/Landmarks/Landmarks/Views/MapView.swift
+++ Ch01_SwiftUI Essentials/Landmarks/Landmarks/Views/MapView.swift
@@ -9,22 +9,38 @@ import SwiftUI
 import MapKit // 맵 뷰를 만들기 위해 추가해줌
 
 struct MapView: View {
-    // 상태 프로퍼티 private로 선언
-    // 이래야 실시간으로 뷰에 업데이트가 됨
-    @State private var region = MKCoordinateRegion( // Mapkit에서 지원해주는
-        // 중앙 위치 설정
-           center:  CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
-           span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
-       )
+    // Circle 처럼
+    var coordinate: CLLocationCoordinate2D
+    @State private var region = MKCoordinateRegion() // 계산한 걸 넣어줌
+    
+//    // 상태 프로퍼티 private로 선언
+//    // 이래야 실시간으로 뷰에 업데이트가 됨
+//    @State private var region = MKCoordinateRegion( // Mapkit에서 지원해주는
+//        // 중앙 위치 설정
+//           center:  CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+//           span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+//       )
     
     var body: some View {
         // $ -> 바인딩
         Map(coordinateRegion: $region)
+            .onAppear {
+                setRegion(coordinate)
+            }
+    }
+    
+    // 내부용 update
+    private func setRegion(_ coordinate: CLLocationCoordinate2D) {
+        region = MKCoordinateRegion(
+            center: coordinate,
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
     }
 }
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        // 바인딩보단 파라미터로 해줄수 있음
+        MapView(coordinate: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868))
     }
 }
